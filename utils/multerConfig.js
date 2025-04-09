eefecd9f638ca6b7a1033fe9d699795dcfc0a333
santhoshreddy792762ksr <santhoshreddykondagari@gmail.com>
2025-04-10 01:30:56 +0530

    Add files via upload

---
+++ utils/multerConfig.js
@@ -0,0 +1,32 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'public/uploads/'); // Make sure this directory exists
+    },
+    filename: function(req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const ext = path.extname(file.originalname);
+        cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    const filetypes = /jpeg|jpg|png|gif/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    
+    if (mimetype && extname) {
+        return cb(null, true);
+    }
+    cb(new Error('Only image files (jpeg, jpg, png, gif) are allowed!'));
+};
+
+const upload = multer({ 
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit
+});
+
+module.exports = upload;
